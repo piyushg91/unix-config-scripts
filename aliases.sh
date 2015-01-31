078c0a443cf981ab8b55539506bfcb587c7cd677
Piyush Gupta <piyushg91@gmail.com>
2015-01-31 15:41:57 -0800

    Added git reset --hard HEAD alias

---
+++ aliases.sh
@@ -0,0 +1,33 @@
+alias stt='git status'
+
+alias gff='git diff'
+
+alias cmt='git commit'
+
+alias oln='git log --oneline --decorate'
+
+alias gph='git push'
+
+alias viewalias='vi ~/prodscripts/aliases.sh'
+
+alias srcme='source ~/.bashrc'
+
+alias clc='clear'
+
+export PS1='$ '
+
+alias adt='~/SDKs/Android-SDK/eclipse/eclipse &'
+
+alias enb='repo init -u git@bitbucket.org:piyushg91/manifests-engelbart'
+
+alias ast='/home/piyush/SDKs/android-studio/bin/studio.sh &'
+
+alias exml="cd .repo/manifests; vi default.xml"
+
+alias djs='python manage.py runserver'
+
+alias cmf='~/prodscripts/mFile_withFunction.sh'
+
+alias ssd="sudo shutdown now"
+
+alias hrst="git reset --hard HEAD"
