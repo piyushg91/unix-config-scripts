eb965a648ab51cb054b2ec0661df5750cba11e42
Piyush Gupta <piyushg91@gmail.com>
2015-02-02 14:13:40 -0800

    Removed some Android aliases, modified some, and added virtualENV paths

--- aliases.sh
+++ aliases.sh
@@ -6,9 +6,9 @@ alias cmt='git commit'
 
 alias oln='git log --oneline --decorate'
 
-alias gph='git push'
+alias gph='git push -q'
 
-alias viewalias='vi ~/prodscripts/aliases.sh'
+alias viewalias='vi ~/unixscripts/aliases.sh'
 
 alias srcme='source ~/.bashrc'
 
@@ -18,16 +18,15 @@ export PS1='$ '
 
 alias adt='~/SDKs/Android-SDK/eclipse/eclipse &'
 
-alias enb='repo init -u git@bitbucket.org:piyushg91/manifests-engelbart'
-
-alias ast='/home/piyush/SDKs/android-studio/bin/studio.sh &'
-
-alias exml="cd .repo/manifests; vi default.xml"
-
 alias djs='python manage.py runserver'
 
-alias cmf='~/prodscripts/mFile_withFunction.sh'
+alias cmf='~/unixcripts/mFile_withFunction.sh'
 
 alias ssd="sudo shutdown now"
 
 alias hrst="git reset --hard HEAD"
+
+#VirtualENV setupt
+export WORKON_HOME=$HOME/envs
+export PROJECT_HOME=$HOME/python_projects
+source /usr/local/bin/virtualenvwrapper.sh
