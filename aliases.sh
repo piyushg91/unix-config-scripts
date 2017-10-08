79b6eb62d671b5ecffcb65e6a9d0aa63e83f4530
Piyush Gupta <piyushg91@gmail.com>
2017-10-08 14:10:04 -0700

    Adding lunar bash settings

--- aliases.sh
+++ aliases.sh
@@ -35,3 +35,15 @@ source /usr/local/bin/virtualenvwrapper.sh
 
 export ANDROID_HOME=~/Android/SDK
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+export ROS_ROOT=/opt/ros/lunar/share/ros
+export ROS_PACKAGE_PATH=/opt/ros/lunar/share
+export ROS_MASTER_URI=http://localhost:11311
+export LD_LIBRARY_PATH=/opt/ros/lunar/lib
+export PATH=/opt/ros/lunar/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export ROSLISP_PACKAGE_DIRECTORIES=
+export ROS_DISTRO=lunar
+export PYTHONPATH=$ROS_ROOT/core/roslib/src:$LD_LIBRARY_PATH/python2.7/dist-packages
+export PKG_CONFIG_PATH=/opt/ros/lunar/lib/pkgconfig
+export CMAKE_PREFIX_PATH=/opt/ros/lunar
+export ROS_ETC_DIR=/opt/ros/lunar/etc/ros
